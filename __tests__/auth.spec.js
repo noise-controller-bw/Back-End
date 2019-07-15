501d519a6a2a5b0b268453a5cf760498a2f96c15
Levi-Huynh <Levi_huynh@yahoo.com>
2019-07-15 15:46:25 -0700

    revised login and registration posts, created all tests for login and registration

--- __tests__/auth.spec.js
+++ __tests__/auth.spec.js
@@ -4,6 +4,7 @@ const server = require("../api/server");
 
 const Users = require("../api/helpers/usersHelper.js");
 
+// REGISTER TESTS
 describe("login and registration routes", () => {
   afterEach(async () => {
     await db("users").truncate();
@@ -23,11 +24,146 @@ describe("login and registration routes", () => {
           lastname: "Smith",
           username: "Msmith",
           password: "test",
-          email: "smith@gmail.com",
+          email: "smith5w@gmail.com",
           role: "teacher"
         });
 
       expect(res.status).toBe(201);
+    });
+
+    it("responds with `Please fill out missing fields!` if field is missing", async () => {
+      beforeEach(async () => {
+        await db("users").truncate();
+      });
+      let res = await supertest(server)
+        .post("/register")
+        .send({
+          firstname: "Matt",
+          lastname: "Smith",
+          password: "test",
+          email: "smith5w@gmail.com",
+          role: "teacher"
+        });
+
+      expect(res.body.message).toEqual("Please fill out missing fields!");
+    });
+
+    it("returns provided user in res body upon succeeding registration", async () => {
+      beforeEach(async () => {
+        await db("users").truncate();
+      });
+
+      let newUser = {
+        firstname: "Matt",
+        lastname: "Smith",
+        username: "Msmith",
+        password: "test",
+        email: "smith5w@gmail.com",
+        role: "teacher"
+      };
+      let res = await supertest(server)
+        .post("/register")
+        .send(newUser);
+
+      expect(res.body.username).toEqual("Msmith");
+    });
+  });
+
+  //LOGIN TESTS
+
+  describe("post /login", () => {
+    //async test need to either return the promise
+    beforeEach(async () => {
+      await db("users").truncate();
+    });
+
+    it("responds with 200 status", async () => {
+      let res = await supertest(server)
+        .post("/register")
+        .send({
+          firstname: "Matt",
+          lastname: "Smith",
+          username: "Msmith",
+          password: "test",
+          email: "smith5w@gmail.com",
+          role: "teacher"
+        });
+      let res2 = await supertest(server)
+        .post("/login")
+        .send({
+          username: "Msmith",
+          password: "test"
+        });
+
+      expect(res2.status).toBe(200);
+    });
+
+    it("responds with welcome message", async () => {
+      beforeEach(async () => {
+        await db("users").truncate();
+      });
+      let res = await supertest(server)
+        .post("/register")
+        .send({
+          firstname: "Matt",
+          lastname: "Smith",
+          username: "Msmith9",
+          password: "test",
+          email: "smith5w@gmail.com",
+          role: "teacher"
+        });
+      let res2 = await supertest(server)
+        .post("/login")
+        .send({
+          username: "Msmith9",
+          password: "test"
+        });
+
+      expect(res2.body.message).toEqual("Welcome Msmith9!");
+    });
+
+    it("responds with status 401 if  password or username is invalid", async () => {
+      let res = await supertest(server)
+        .post("/register")
+        .send({
+          firstname: "Matt",
+          lastname: "Smith",
+          username: "Msmith",
+          password: "test",
+          email: "smith5w@gmail.com",
+          role: "teacher"
+        });
+      let res2 = await supertest(server)
+        .post("/login")
+        .send({
+          username: "Msmith",
+          password: "pasword"
+        });
+
+      expect(res2.body.message).toEqual("Invalid Credentials");
+    });
+
+    it("responds with `please fill out missing fields` if missing username or password", async () => {
+      beforeEach(async () => {
+        await db("users").truncate();
+      });
+      let res = await supertest(server)
+        .post("/register")
+        .send({
+          firstname: "Matt",
+          lastname: "Smith",
+          username: "Msmith",
+          password: "test",
+          email: "smith5w@gmail.com",
+          role: "teacher"
+        });
+      let res2 = await supertest(server)
+        .post("/login")
+        .send({
+          password: "pasword"
+        });
+
+      expect(res2.body.message).toEqual("Please fill out missing fields!");
     });
   });
 });
--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 
-const { authenticate, tokenAuthen } = require("../../auth/authenticate.js");
+const { authenticate, generateToken } = require("../../auth/authenticate.js");
 
 const { addUser, getUserByFilter } = require("../helpers");
 
@@ -22,23 +22,26 @@ router.post("/register", (req, res) => {
 //Todo: Add authenticate mw to ensure user is authenticated
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
-
-  getUserByFilter({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = tokenAuthen.generateToken(user);
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-          token
-        });
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  if (!username || !password) {
+    return res.status(422).json({ message: "Please fill out missing fields!" });
+  } else {
+    getUserByFilter({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+            token
+          });
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  }
 });
 
 module.exports = router;
