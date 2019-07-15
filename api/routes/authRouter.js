ae313090e3395d2d85e1bcf7bcc9ac4b67e7ffa3
Levi-Huynh <Levi_huynh@yahoo.com>
2019-07-15 09:36:23 -0700

    moved login and registration to authRouther.js

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -1,43 +1,43 @@
-// const router = require("express").Router();
-// const bcrypt = require("bcryptjs");
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-// const { authenticate, tokenAuthen } = require("../../auth/authenticate.js");
+const { authenticate, tokenAuthen } = require("../../auth/authenticate.js");
 
-// const { addUser, getUserByFilter } = require("../helpers");
+const { addUser, getUserByFilter } = require("../helpers");
 
-// router.post("/register", (req, res) => {
-//   let user = req.body;
-//   const hash = bcrypt.hashSync(user.password, 10);
-//   user.password = hash;
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
 
-//   addUser(user)
-//     .then(saved => {
-//       res.status(201).json(saved);
-//     })
-//     .catch(error => {
-//       res.status(500).json(error);
-//     });
-// });
+  addUser(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
-// router.post("/login", (req, res) => {
-//   let { username, password } = req.body;
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
 
-//   getUserByFilter({ username })
-//     .first()
-//     .then(user => {
-//       if (user && bcrypt.compareSync(password, user.password)) {
-//         const token = tokenAuthen.generateToken(user);
-//         res.status(200).json({
-//           message: `Welcome ${user.username}!`,
-//           token
-//         });
-//       } else {
-//         res.status(401).json({ message: "Invalid Credentials" });
-//       }
-//     })
-//     .catch(error => {
-//       res.status(500).json(error);
-//     });
-// });
+  getUserByFilter({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenAuthen.generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
-// module.exports = router;
+module.exports = router;
